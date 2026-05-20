1af7e08811c3d029897264801b9c2c6aef08513d
carlosrod1987-tech <>
2026-05-20 01:47:04 +0000

    Nested Lists

---
+++ other_data_types/nested_lists/main.py
@@ -0,0 +1,18 @@
+#Create a variable vegetables with the list ["tomatoes", "potatoes", "onions"]
+vegetables = ["tomatoes", "potatoes", "onions"]
+
+#Remove "onions" from the list.
+vegetables.remove("onions")
+print("Updated Vegetable Inventory:", vegetables)
+
+#Add "carrots" to the list.
+vegetables.append("carrots")
+print("Carrots are already in the list.", vegetables)
+
+#Add "cucumbers" to the list.
+vegetables.append("cucumbers")
+print("Cucumbers are already in the list.", vegetables)
+
+#Sort the list alphabetically.
+vegetables.sort()
+
