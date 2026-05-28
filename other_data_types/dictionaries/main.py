fcf4f63daed254d116e9b7d2c67a4da0aed54add
carlosrod1987-tech <>
2026-05-28 04:08:31 +0000

    Dictionaries

---
+++ other_data_types/dictionaries/main.py
@@ -0,0 +1,12 @@
+#Define a dictionary grocery_inventory
+grocery_inventory = {"Milk": (113, "Dairy"), "Eggs": (116, "Dairy"), "Bread": (117, "Bakery"), "Apples": (141, "Produce")}
+bread_details = grocery_inventory.get("Bread")
+print("Details of Bread:", bread_details)
+
+#add new item "Cookies" into the grocery_inventory
+grocery_inventory.update({"Cookies": (143, "Bakery")})
+print("Inventory after adding Cookies:", grocery_inventory)
+
+#remove "Eggs" from Dictionary
+grocery_inventory.pop("Eggs")
+print("Inventory after removing Eggs:", grocery_inventory)
